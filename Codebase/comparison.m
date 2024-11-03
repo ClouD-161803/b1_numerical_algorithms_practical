cbd0a045065816e1b3417880afdcd78c1ab55be9
claudio vestini <cvestini@gmail.com>
2024-11-03 15:57:04 +0000

    Add comparison plot

---
+++ Codebase/comparison.m
@@ -0,0 +1,44 @@
+% Compare Pi Estimation Methods
+clc; clear; close all;
+
+% Parameters
+num_of_num_samples = 100;
+lower_bound = 1;
+upper_bound = 100;
+samples = linspace(lower_bound, upper_bound, num_of_num_samples);
+
+% Methods
+methods = {@montecarlo, @bisection, @newton_raphson, @ramanujan};
+method_names = {'Monte Carlo', 'Bisection', 'Newton Raphson', 'Ramanujan'};
+errors = zeros(num_of_num_samples, length(methods));
+
+% Calculate errors for each method
+for m = 1:length(methods)
+    estimation_method = methods{m};
+    for idx = 1:num_of_num_samples
+        
+        N = round(samples(idx));
+        if m == 1
+            % Monte Carlo method
+            num_samples = N * 1000;
+            pi_est = estimation_method(num_samples);
+        else
+            % Other methods
+            pi_est = estimation_method(N);
+        end
+        errors(idx, m) = abs(pi_est - pi);
+    end
+end
+
+% Plot results
+figure;
+hold on;
+for m = 1:length(methods)
+    loglog(linspace(lower_bound, upper_bound, num_of_num_samples) * 1000, errors(:, m), 'DisplayName', method_names{m});
+end
+xlabel('Number of Samples / Iterations');
+ylabel('Error');
+title('Comparison of Pi Estimation Methods');
+legend('show');
+grid on;
+hold off;
