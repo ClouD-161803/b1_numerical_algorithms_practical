05373988e1957e8a4406f21ce5cb3fc02b424375
claudio vestini <cvestini@gmail.com>
2024-11-07 12:30:20 +0000

    Add error bars

--- Codebase/plot_results.m
+++ Codebase/plot_results.m
@@ -1,6 +1,9 @@
 function plot_results(num_samples, estimated_pi)
     errors = estimated_pi - pi;
     
+    sample_variance = calculate_variance(errors);
+    error_bars = sqrt(sample_variance) * ones(size(errors));
+    
     figure;
     hold on;
     scatter(num_samples, errors, 'b.');
@@ -10,10 +13,12 @@ function plot_results(num_samples, estimated_pi)
         line([num_samples(i), num_samples(i)], [0, errors(i)], 'Color', 'b', 'LineStyle', '-');
     end
     
+    errorbar(num_samples, errors, error_bars, 'b', 'LineStyle', 'none');
+    
     xlabel('Number of Samples');
     ylabel('Error');
     title('Pi Estimation Error');
-    legend('Motecarlo');
+    legend('Monte Carlo');
     grid on;
     hold off;
 end
