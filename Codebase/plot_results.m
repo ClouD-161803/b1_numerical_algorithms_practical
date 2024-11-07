198ae843752fb1af9aa19d1e8a639eebe867718e
claudio vestini <cvestini@gmail.com>
2024-11-07 12:33:25 +0000

    Modify plotter

--- Codebase/plot_results.m
+++ Codebase/plot_results.m
@@ -2,23 +2,28 @@ function plot_results(num_samples, estimated_pi)
     errors = estimated_pi - pi;
     
     sample_variance = calculate_variance(errors);
-    error_bars = sqrt(sample_variance) * ones(size(errors));
+    sigma = sqrt(sample_variance);
     
     figure;
     hold on;
+    
+    % Plot the estimated errors
     scatter(num_samples, errors, 'b.');
+    
+    % Plot the 1sigma region
+    fill([num_samples, fliplr(num_samples)], ...
+         [errors + sigma, fliplr(errors - sigma)], ...
+         'b', 'FaceAlpha', 0.2, 'EdgeColor', 'none');
     
     % Plot vertical lines from the x-axis to each point
     for i = 1:length(num_samples)
         line([num_samples(i), num_samples(i)], [0, errors(i)], 'Color', 'b', 'LineStyle', '-');
     end
     
-    errorbar(num_samples, errors, error_bars, 'b', 'LineStyle', 'none');
-    
     xlabel('Number of Samples');
     ylabel('Error');
-    title('Pi Estimation Error');
-    legend('Monte Carlo');
+    title('Pi Estimation Error with Sigma Region');
+    legend('Monte Carlo', '1\sigma Region');
     grid on;
     hold off;
 end
